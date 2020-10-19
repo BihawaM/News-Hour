0ce3e18c919eae5c345d55291e8518b3922ac237
BihawaM <bihawam271@gmail.com>
2020-10-19 11:25:44 +0300

    exported news and secrete key in start file from the root folder

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-# export NEWS_API_KEY=5136884436074293b9fd97e1a6c1032d
-# export SECRET_KEY=Bih20
+export NEWS_API_KEY=5136884436074293b9fd97e1a6c1032d
+export SECRET_KEY=Bih20
 
-# python3.6 manage.py server
+python3.6 manage.py server
