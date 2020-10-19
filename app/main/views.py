23534ea08938d7806d80701e81f792624f4543e1
BihawaM <bihawam271@gmail.com>
2020-10-19 11:03:32 +0300

    added articles page definition in views file from main folder

--- app/main/views.py
+++ app/main/views.py
@@ -16,20 +16,14 @@ def index():
         return render_template('index.html', technology = technology, trending = sources, politics = politics, sports = sports, beauty = beauty)
 
 
-
-
-
-
-
-
-# @main.route('/articles', methods=["POST", "GET"])
-# def articles_page():
-#     if request.method == 'POST':
-#         search = request.form.get("search")
-#         articles = requests.get_articles(search)
-#     else:
-#         articles = requests.get_articles("tech")
-#     return render_template('articles.html', articles=articles)
+@main.route('/articles', methods=["POST", "GET"])
+def articles_page():
+    if request.method == 'POST':
+        search = request.form.get("search")
+        articles = requests.get_articles(search)
+    else:
+        articles = requests.get_articles("tech")
+    return render_template('articles.html', articles=articles)
 
 
 # @main.route('/article/<id>')
