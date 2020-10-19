3b20d6b533ca4a7102617072503ca4ea69335643
BihawaM <bihawam271@gmail.com>
2020-10-19 10:43:31 +0300

    added get sources in request file

--- app/request.py
+++ app/request.py
@@ -1,26 +1,26 @@
-# import urllib.request,json
-# from .models import Sources,Article
+import urllib.request,json
+from .models import Sources,Article
 
-# api_key = None
+api_key = None
 
-# base_url = None
+base_url = None
 
-# def configure_request(app):
-#     global api_key,base_url
-#     api_key = app.config['NEWS_API_KEY']
-#     base_url = app.config['NEWS_API_BASE_URL']
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
 
-# def get_sources(sources):
-#     get_sources_url = base_url.format(sources,api_key)
-#     with urllib.request.urlopen(get_sources_url) as url:
-#         get_sources_data = url.read()
-#         get_sources_response = json.loads(get_sources_data)
+def get_sources(sources):
+    get_sources_url = base_url.format(sources,api_key)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
 
-#         sources_results = None
+        sources_results = None
 
-#         if get_sources_response['articles']:
-#             sources_results_list = get_sources_response['articles']
-#             sources_results = process_results(sources_results_list)
+        if get_sources_response['articles']:
+            sources_results_list = get_sources_response['articles']
+            sources_results = process_results(sources_results_list)
 
 
 #     return sources_results
